2a57da7a796bc98ee6a140cab12c9607341682bb
sowmik-sec <sowmik.sec@gmail.com>
2024-03-30 11:08:46 +0600

    complete cloudinary upload utility

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -10,12 +10,13 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
-    // upload the file on cloudinary
+    //upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    // file has been uploaded successfully
-    console.log("file is uploaded on cloudinary ", response.url);
+    // file has been uploaded successfull
+    //console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
@@ -23,10 +24,4 @@ const uploadOnCloudinary = async (localFilePath) => {
   }
 };
 
-cloudinary.uploader.upload(
-  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" },
-  function (error, result) {
-    console.log(result);
-  }
-);
+export { uploadOnCloudinary };

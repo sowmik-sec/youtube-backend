00ecb69691ca7a36b15f61ad0722908bef9e5458
sowmik-sec <sowmik.sec@gmail.com>
2024-03-31 12:07:22 +0600

    create login user functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -84,4 +84,48 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  // req body -> data
+  // username or email
+  // find the user
+  // password check
+  // access and refresh token
+  // send cookie
+
+  const { email, username, password } = req.body;
+  if (!username && !email) {
+    throw new ApiError(400, "username or password is required");
+  }
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return (
+    res
+      .status(200)
+      .cookie("accessToken", options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(200, { user: loggedInUser, accessToken, refreshToken })
+      ),
+    "User logged in successfully"
+  );
+});
+
+export { registerUser, loginUser };

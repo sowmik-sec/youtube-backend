c256d31cb8266f2659d50b54f89bc6b9125c9750
sowmik-sec <sowmik.sec@gmail.com>
2024-04-03 10:24:19 +0600

    bug fixed: add await and new ApiResponse

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -209,7 +209,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -217,7 +217,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
   if (!fullName || !email) {
     throw new ApiError(400, "Full name and email are required");
   }
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {

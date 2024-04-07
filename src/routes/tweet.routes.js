05fcbdf7c92c9a67dd146d9f0f2d10e83368be87
sowmik-sec <sowmik.sec@gmail.com>
2024-04-07 15:35:02 +0600

    Add updateTweet route to tweet.routes.js

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { createTweet, getUserTweets } from "../controllers/tweet.controller.js";
+import {
+  createTweet,
+  getUserTweets,
+  updateTweet,
+} from "../controllers/tweet.controller.js";
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -9,5 +13,7 @@ router.use(verifyJWT); // protect all routes in this file
 router.route("/").post(createTweet);
 
 router.route("/user/:userId").get(getUserTweets);
+
+router.route("/:tweetId").patch(updateTweet);
 
 export default router;

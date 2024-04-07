9526ad4757cfc7b2e22e6091ba73aa8fe4b74f7b
sowmik-sec <sowmik.sec@gmail.com>
2024-04-07 11:22:52 +0600

    remove username from updateAccountDetails controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -218,7 +218,7 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
-  const { fullName, email, username } = req.body;
+  const { fullName, email } = req.body;
   if (!fullName || !email) {
     throw new ApiError(400, "Full name and email are required");
   }

e679128de3409fa3ed214c6dbf5f11a65624c429
sowmik-sec <sowmik.sec@gmail.com>
2024-04-07 15:40:17 +0600

    Add deleteTweet route to tweet.routes.js

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createTweet,
+  deleteTweet,
   getUserTweets,
   updateTweet,
 } from "../controllers/tweet.controller.js";
@@ -14,6 +15,6 @@ router.route("/").post(createTweet);
 
 router.route("/user/:userId").get(getUserTweets);
 
-router.route("/:tweetId").patch(updateTweet);
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 
 export default router;

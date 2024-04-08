2bffdef9c332a32a98b81f88d41339ed788b26c6
sowmik-sec <sowmik.sec@gmail.com>
2024-04-08 21:12:34 +0600

    Refactor aggregation pipeline to include author details lookup in .hritesh.aggregation.js

--- .hritesh.aggregation.js
+++ .hritesh.aggregation.js
@@ -139,3 +139,21 @@
     },
   },
 ];
+
+[
+  {
+    $lookup: {
+      from: "authors",
+      localField: "author_id",
+      foreignField: "_id",
+      as: "author_details",
+    },
+  },
+  {
+    $addFields: {
+      author_details: {
+        $arrayElemAt: ["$author_details", 0],
+      },
+    },
+  },
+];

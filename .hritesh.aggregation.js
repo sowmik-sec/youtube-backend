af64375d35562f7c16dfce10a35aa6c93bae53d5
sowmik-sec <sowmik.sec@gmail.com>
2024-04-08 17:24:44 +0600

    Add aggregation pipeline for calculating average number of tags in .hritesh.aggregation.js

---
+++ .hritesh.aggregation.js
@@ -0,0 +1,21 @@
+[
+  {
+    $unwind: "$tags",
+  },
+  {
+    $group: {
+      _id: "$_id",
+      numberOfTags: {
+        $sum: 1,
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      averageNumberOfTags: {
+        $avg: "$numberOfTags",
+      },
+    },
+  },
+];

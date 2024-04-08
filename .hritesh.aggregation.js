59c66bbae764a81a813bdaca8818e7b71e8f0f84
sowmik-sec <sowmik.sec@gmail.com>
2024-04-08 20:45:54 +0600

    Refactor aggregation pipeline to include sorting, limiting, and projecting in .hritesh.aggregation.js

--- .hritesh.aggregation.js
+++ .hritesh.aggregation.js
@@ -77,3 +77,21 @@
     $count: "usersWithSpecialPhoneNumber",
   },
 ];
+
+[
+  {
+    $sort: {
+      registered: -1,
+    },
+  },
+  {
+    $limit: 4,
+  },
+  {
+    $project: {
+      name: 1,
+      registered: 1,
+      favoriteFruit: 1,
+    },
+  },
+];

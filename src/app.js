2f903d35561dae7823af49f2735c4d39cedf8fad
sowmik-sec <sowmik.sec@gmail.com>
2024-04-14 18:19:56 +0600

    Add subscription.routes.js and subscription.controller.js to handle subscription functionality

--- src/app.js
+++ src/app.js
@@ -27,9 +27,11 @@ app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 export { app };

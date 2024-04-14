05e25703613d2f64be7b60077dea4f670e90175f
sowmik-sec <sowmik.sec@gmail.com>
2024-04-14 18:19:41 +0600

    Refactor subscription.controller.js to remove duplicate import and add getUserChannelSubscribers function

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,7 +1,6 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { User } from "../models/user.model.js";
 import { Subscription } from "../models/subscription.model.js";
-import { Subscription } from "../models/subscription.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -36,4 +35,24 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     );
 });
 
-export { toggleSubscription };
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id");
+  }
+  const channel = await User.findById(channelId);
+  if (!channel) {
+    throw new ApiError(404, "Channel not found");
+  }
+  const subscribers = await Subscription.find({ channel: channelId }).populate(
+    "subscriber",
+    "username"
+  );
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, subscribers, "Subscribers fetched successfully")
+    );
+});
+
+export { toggleSubscription, getUserChannelSubscribers };

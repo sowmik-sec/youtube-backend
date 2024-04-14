c818f3bbeeb638faa70d301bb25a9a5f4a7c19d2
sowmik-sec <sowmik.sec@gmail.com>
2024-04-14 17:40:20 +0600

    Add subscription.controller.js to handle subscription toggling

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,31 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+  const { userId } = req.user;
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id");
+  }
+  const channel = await User.findById(channelId);
+  if (!channel) {
+    throw new ApiError(404, "Channel not found");
+  }
+  const subscription = await Subscription.findOne({
+    subscriber: userId,
+    channel: channelId,
+  });
+  if (subscription) {
+    await subscription.remove();
+    return res.json(new ApiResponse(200, "Unsubscribed successfully"));
+  }
+  await Subscription.create({ subscriber: userId, channel: channelId });
+  return res.json(new ApiResponse(200, "Subscribed successfully"));
+});
+
+export { toggleSubscription };

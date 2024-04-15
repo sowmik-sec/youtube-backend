6f33d5b0d9b2db22c25689875580ce081d77535e
sowmik-sec <sowmik.sec@gmail.com>
2024-04-15 16:35:19 +0600

    Add getUserChannelSubscribers route to subscription.routes.js

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import {
+  getUserChannelSubscribers,
+  toggleSubscription,
+} from "../controllers/subscription.controller.js";
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -8,5 +11,7 @@ const router = Router();
 router.use(verifyJWT);
 
 router.route("/c/:channelId").post(toggleSubscription);
+
+router.route("/u/:subscriberId").get(getUserChannelSubscribers);
 
 export default router;

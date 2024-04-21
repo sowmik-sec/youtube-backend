6b5252e0b8ed60a156c8738fd90c46e68811fce8
sowmik-sec <sowmik.sec@gmail.com>
2024-04-21 11:32:22 +0600

    Add publishAVideo function to video.controller.js

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -24,4 +24,49 @@ const getAllVideos = asyncHandler(async (req, res) => {
   return res.json(new ApiResponse(videos));
 });
 
-export { getAllVideos };
+const publishAVideo = asyncHandler(async (req, res) => {
+  const { title, description } = req.body;
+  if ([title, description].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All are required");
+  }
+  const videoFileLocalPath = req.files?.videoFile[0]?.path;
+  const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+  if (!videoFileLocalPath) {
+    throw new ApiError(400, "VideoFileLocalPath is required");
+  }
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "ThumbnailLocalPath is required");
+  }
+  const videoFile = await uploadOnCloudinary(videoFileLocalPath);
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+  if (!videoFile) {
+    throw new ApiError(500, "Failed to upload video file");
+  }
+  if (!thumbnail) {
+    throw new ApiError(500, "Failed to upload thumbnail");
+  }
+  const video = await Video.create({
+    title,
+    description,
+    duration: videoFile.duration,
+    videoFile: {
+      url: videoFile.url,
+      public_id: videoFile.public_id,
+    },
+    thumbnail: {
+      url: thumbnail.url,
+      public_id: thumbnail.public_id,
+    },
+    owner: req.user?._id,
+    isPublished: false,
+  });
+  const videoUploaded = await Video.findById(video._id);
+  if (!videoUploaded) {
+    throw new ApiError(500, "VideoUpload failed. please try again");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video uploaded successfully"));
+});
+
+export { getAllVideos, publishAVideo };

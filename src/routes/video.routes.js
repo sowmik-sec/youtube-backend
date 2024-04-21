9f34997813bb094b0d568a6b9b2860a7a7af0b8d
sowmik-sec <sowmik.sec@gmail.com>
2024-04-21 11:32:29 +0600

    Add video upload functionality to video.routes.js

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -2,12 +2,27 @@ import { Router } from "express";
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { getAllVideos } from "../controllers/video.controller";
+import { getAllVideos, publishAVideo } from "../controllers/video.controller";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("/").get(getAllVideos);
+router
+  .route("/")
+  .get(getAllVideos)
+  .post(
+    upload.fields([
+      {
+        name: "videoFile",
+        maxCount: 1,
+      },
+      {
+        name: "thumbnail",
+        maxCount: 1,
+      },
+    ]),
+    publishAVideo
+  );
 
 export default router;
